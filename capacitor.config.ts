f65ef17a2194d1fb3d7717cfd4b4140de9c17453
nflintel <nflintel.com@gmail.com>
2026-03-28 01:22:41 -0400

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dna.dirtnapp.com',
+  appName: 'ArtDNA',
+  webDir: 'dist'
+};
+
+export default config;
